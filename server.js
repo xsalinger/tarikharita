1d83b13beb79f5a1e61b59273829c8b1c1335c23
xsalinger <mustafataylanbektas@hotmail.com>
2025-06-09 18:43:56 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda çalışıyor.`);
+});
